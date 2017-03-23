5f4f7cb37c6a57a68c0513fcd2b67cd3a7a9bac9
David Christie <davidchristie.nz@gmail.com>
2017-03-23 14:37:42 +1300

    Refactor finding cheapest suburb

--- src/selectors/suburbs.js
+++ src/selectors/suburbs.js
@@ -1,3 +1,14 @@
+export function getCheapestSelectedSuburb (state) {
+  const selected = getSelectedSuburbs(state)
+  const prices = selected.map(suburb => {
+    return suburb.price
+  })
+  const min = Math.min(...prices)
+  return selected.find(suburb => {
+    return suburb.price === min
+  })
+}
+
 export function getSelectedSuburbs (state) {
   const values = state.form.location.values || {}
   const selected = values.suburbs || []
