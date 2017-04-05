4b6cbb5ec8facef6584ef3784c3b50981c7fadd3
David Christie <davidchristie.nz@gmail.com>
2017-04-05 21:20:05 +1200

    Add links to privacy page

--- src/components/App/Footer/Footer.js
+++ src/components/App/Footer/Footer.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react'
 import { Col, Grid, Panel } from 'react-bootstrap'
+import { Link } from 'react-router'
+
+import './Footer.css'
 
 class Footer extends Component {
   render () {
     return (
-      <Grid>
+      <Grid className='Footer'>
         <Col md={2} />
         <Col md={8}>
           <Panel style={{textAlign: 'center'}}>
-            <p>
-              <strong>
-                Images &copy; Noah Fagan
-              </strong>
-            </p>
+            <ul>
+              <li>
+                <Link to='/privacy'>Privacy</Link>
+              </li>
+            </ul>
+            <strong>
+              &copy; 2017 Kaenga
+            </strong>
+            <br />
+            <strong>
+              Images &copy; Noah Fagan
+            </strong>
           </Panel>
         </Col>
         <Col md={2} />
