e18614d4f9bab8f5577045cadbb5a51e86ccaa08
David Christie <davidchristie.nz@gmail.com>
2017-04-07 13:18:34 +1200

    round-currency-values

--- src/components/units/Currency/Currency.js
+++ src/components/units/Currency/Currency.js
@@ -6,9 +6,28 @@ import { FormattedNumber } from 'react-intl'
 class Currency extends Component {
   render () {
     const { type, value } = this.props
-    return (
-      <FormattedNumber currency={type} style='currency' value={value} />
-    )
+    if (value > 1000) {
+      return (
+        <span>
+          <FormattedNumber
+            currency={type}
+            maximumSignificantDigits={2}
+            style='currency'
+            value={value / 1000}
+          />
+          K
+        </span>
+      )
+    } else {
+      return (
+        <FormattedNumber
+          currency={type}
+          maximumSignificantDigits={2}
+          style='currency'
+          value={value}
+        />
+      )
+    }
   }
 }
 
