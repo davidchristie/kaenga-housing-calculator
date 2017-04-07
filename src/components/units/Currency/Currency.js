ad1f286cd168a717147d8535d50231a1985264b7
David Christie <davidchristie.nz@gmail.com>
2017-04-07 14:01:11 +1200

    Simplify curreny values

--- src/components/units/Currency/Currency.js
+++ src/components/units/Currency/Currency.js
@@ -1,18 +1,15 @@
-/* eslint-disable react/style-prop-object */
-
 import React, { Component, PropTypes } from 'react'
 import { FormattedNumber } from 'react-intl'
 
 class Currency extends Component {
   render () {
-    const { type, value } = this.props
+    const { value } = this.props
     if (value > 1000) {
       return (
         <span>
+          $
           <FormattedNumber
-            currency={type}
             maximumSignificantDigits={2}
-            style='currency'
             value={value / 1000}
           />
           K
@@ -20,24 +17,23 @@ class Currency extends Component {
       )
     } else {
       return (
-        <FormattedNumber
-          currency={type}
-          maximumSignificantDigits={2}
-          style='currency'
-          value={value}
-        />
+        <span>
+          $
+          <FormattedNumber
+            maximumSignificantDigits={2}
+            value={value}
+          />
+        </span>
       )
     }
   }
 }
 
 Currency.defaultProps = {
-  type: 'nzd',
   value: 0
 }
 
 Currency.propTypes = {
-  type: PropTypes.string.isRequired,
   value: PropTypes.number.isRequired
 }
 
