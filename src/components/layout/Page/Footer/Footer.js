d4f2fcba0dae44cc738cfd138f105213a874c7a4
David Christie <davidchristie.nz@gmail.com>
2017-04-26 13:37:17 +1200

    Update footer

--- src/components/layout/Page/Footer/Footer.js
+++ src/components/layout/Page/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Col, Grid, Panel } from 'react-bootstrap'
+import { Well } from 'react-bootstrap'
 import { Link } from 'react-router'
 
 import './Footer.css'
@@ -7,26 +7,18 @@ import './Footer.css'
 class Footer extends Component {
   render () {
     return (
-      <Grid className='Footer'>
-        <Col md={2} />
-        <Col md={8}>
-          <Panel style={{textAlign: 'center'}}>
-            <ul>
-              <li>
-                <Link to='/privacy'>Privacy</Link>
-              </li>
-            </ul>
+      <Well className='Footer' style={{textAlign: 'center'}}>
+        <ul>
+          <li>
             <strong>
               &copy; 2017 Kaenga
             </strong>
-            <br />
-            <strong>
-              Images &copy; Noah Fagan
-            </strong>
-          </Panel>
-        </Col>
-        <Col md={2} />
-      </Grid>
+          </li>
+          <li>
+            <Link to='/privacy'>Privacy</Link>
+          </li>
+        </ul>
+      </Well>
     )
   }
 }
