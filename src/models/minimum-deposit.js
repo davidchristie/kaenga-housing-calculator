db9b7c4ee81489c941081a51b88dc22d13bc51ef
David Christie <davidchristie.nz@gmail.com>
2017-05-10 14:19:29 +1200

    Update minimum deposit calculation

--- src/models/minimum-deposit.js
+++ src/models/minimum-deposit.js
@@ -2,5 +2,7 @@ import { getPriceRange } from '../selectors/report'
 
 export default state => {
   const minTotalCost = getPriceRange(state).min
-  return minTotalCost * 0.2
+  return minTotalCost < 650000
+    ? minTotalCost * 0.1
+    : minTotalCost * 0.2
 }
