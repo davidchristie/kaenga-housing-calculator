4472a065dee6c2dd55baf98b94831787ed1eeb17
David Christie <davidchristie.nz@gmail.com>
2017-05-26 13:01:14 +1200

    Store redux state in local storage

--- src/store.js
+++ src/store.js
@@ -6,9 +6,28 @@ import rootReducer from './reducers'
 const extensionCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
 const composeEnhancers = extensionCompose || compose
 
-export default createStore(
+let storedStateJson = null
+try {
+  storedStateJson = window.localStorage.getItem('state')
+} catch (error) {
+  console.log(error.message)
+  window.localStorage.clear()
+}
+
+const preloadedState = storedStateJson
+  ? JSON.parse(storedStateJson)
+  : {}
+
+const store = createStore(
   rootReducer,
+  preloadedState,
   composeEnhancers(
     applyMiddleware(thunk)
   )
 )
+
+store.subscribe(() => {
+  window.localStorage.setItem('state', JSON.stringify(store.getState()))
+})
+
+export default store
