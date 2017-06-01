cba230329e89c6d39020a29b17937d536c6452f2
David Christie <davidchristie.nz@gmail.com>
2017-06-01 17:51:45 +1200

    Add feature images

--- src/containers/reports/Features/Features.js
+++ src/containers/reports/Features/Features.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react'
-import { Grid, ListGroup, Well } from 'react-bootstrap'
+import { Carousel, Grid, Well } from 'react-bootstrap'
 import { connect } from 'react-redux'
 
 import features from '../../../selectors/features'
-import Feature from './Feature'
 
 class Features extends Component {
   render () {
     const { selected } = this.props
     return (
       <Well>
-        <Grid>
-          <h2>Shared Features</h2>
-          <ListGroup>
-            {selected.map((feature, index) => {
-              return <Feature key={index} {...feature} />
-            })}
-          </ListGroup>
-        </Grid>
+        <h2>Shared Features</h2>
+        <Carousel>
+          {selected.map((feature, index) => {
+            return (
+              <Carousel.Item className='Feature' key={index}>
+                <div>
+                  <img
+                    alt='900x500'
+                    height={500}
+                    src={feature.image || 'http://placehold.it/900x500'}
+                  />
+                </div>
+                <Carousel.Caption>
+                  <h3>{feature.text}</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+            )
+          })}
+        </Carousel>
       </Well>
     )
   }
--- src/models/feature-list.js
+++ src/models/feature-list.js
@@ -4,15 +4,31 @@ import lifestyle from '../selectors/lifestyle'
 import * as landCost from '../selectors/land-cost'
 import sustainability from '../selectors/sustainability'
 
+import artistStudioImage from './feature-images/Artist studio.jpeg'
+import gymImage from './feature-images/Gym and yoga studio.jpg'
+import gardenImage from './feature-images/Rooftop veggie garden.jpg'
+import avRoomImage from './feature-images/AV room for games and movies.jpg'
+import libraryImage from './feature-images/Library and study space.jpeg'
+import smartSustainabilityImage from './feature-images/Smart sustainability features such as insulation, double glazing, and waste minimization.jpeg'
+import publicTransportImage from './feature-images/Close to public transport.jpeg'
+import workshopImage from './feature-images/Makers space and workshop.jpg'
+import sustainabilityImage from './feature-images/Sustainability picture.jpg'
+import entertainmentImage from './feature-images/Entertainment space for hosting large gatherings.jpg'
+import musicImage from './feature-images/Music Room.jpeg'
+import storageImage from './feature-images/Ground floor storage for bikes, kayaks, etc.jpeg'
+import carsharingImage from './feature-images/Onsite carsharing scheme.jpeg'
+
 export default [
   {
     condition: state => true,
     cost: state => ({max: 0, min: 0}),
+    image: carsharingImage,
     text: 'Onsite car share service'
   },
   {
     condition: state => true,
     cost: state => ({max: 0, min: 0}),
+    image: publicTransportImage,
     text: 'Close to public transport'
   },
   {
@@ -30,6 +46,7 @@ export default [
   {
     condition: state => lifestyle(state).sports,
     cost: state => ({max: 2000, min: 2000}),
+    image: storageImage,
     text: 'Plenty of storage for your sports equipment'
   },
   {
@@ -42,6 +59,7 @@ export default [
           landCost.getPerSqmRange(state).min)
       }
     },
+    image: gymImage,
     tags: ['multipurpose'],
     text: 'Onsite gym and yoga studio'
   },
@@ -55,11 +73,13 @@ export default [
           landCost.getPerSqmRange(state).min)
       }
     },
+    image: workshopImage,
     text: 'Communal workshop/makers space'
   },
   {
     condition: state => lifestyle(state).gardening,
     cost: state => ({max: 10000, min: 10000}),
+    image: gardenImage,
     text: 'Roof top flower and vegie garden'
   },
   {
@@ -72,6 +92,7 @@ export default [
           landCost.getPerSqmRange(state).min)
       }
     },
+    image: avRoomImage,
     tags: ['multipurpose'],
     text: 'A communal games room'
   },
@@ -85,6 +106,7 @@ export default [
           landCost.getPerSqmRange(state).min)
       }
     },
+    image: libraryImage,
     tags: ['multipurpose'],
     text: 'A communal study/reading room'
   },
@@ -98,6 +120,7 @@ export default [
           landCost.getPerSqmRange(state).min)
       }
     },
+    image: musicImage,
     tags: ['multipurpose'],
     text: 'A music room'
   },
@@ -111,6 +134,7 @@ export default [
           landCost.getPerSqmRange(state).min)
       }
     },
+    image: artistStudioImage,
     tags: ['multipurpose'],
     text: 'An onsite artist studio'
   },
@@ -124,21 +148,25 @@ export default [
           landCost.getPerSqmRange(state).min)
       }
     },
+    image: entertainmentImage,
     text: 'Indoor and outdoor entertainment spaces with a high end kitchen and barbeque'
   },
   {
-    condition: state => true,
+    condition: state => sustainability(state).level === 'low',
     cost: state => ({max: 0, min: 0}),
-    text: 'North facing, double glazing, solar water heating'
+    image: sustainabilityImage,
+    text: 'Sensible sustainability measure like orienting the building north, reducing waste and insulating your home well.'
   },
   {
-    condition: state => ['medium', 'high'].includes(sustainability(state).level),
+    condition: state => sustainability(state).level === 'medium',
     cost: state => ({max: 0, min: 0}),
-    text: 'Water Reticulation and water efficient applicances'
+    image: smartSustainabilityImage,
+    text: 'Smart energy efficient technology that will save you money in the long run.'
   },
   {
     condition: state => sustainability(state).level === 'high',
     cost: state => ({max: 0, min: 0}),
-    text: 'Photovoltaic panels and native planting'
+    image: smartSustainabilityImage,
+    text: 'Your home will be built to the rigorous Passivhaus standard.'
   }
 ]
